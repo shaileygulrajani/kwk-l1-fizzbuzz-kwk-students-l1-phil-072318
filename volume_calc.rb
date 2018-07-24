7be75e5ed095d646b9096c359dc5eca47657b83c
shaileygulrajani <shaileygulrajani@gmail.com>
2018-07-24 18:20:45 +0000

    automatically backed up by learn

--- volume_calc.rb
+++ volume_calc.rb
@@ -2,8 +2,11 @@ puts "What is the radius?"
 radius = gets.chomp.to_i
 
 puts "Is this a circle or a sphere?"
-shape = gets.chomp
+shape = gets.chomp.downcase
 
 if shape == "circle" 
 puts "Your area is #{3.14*radius*radius}"
-end 
+
+elsif shape == "sphere"
+puts "Your area is #{(4/3)*3.14*radius*radius}"
+end

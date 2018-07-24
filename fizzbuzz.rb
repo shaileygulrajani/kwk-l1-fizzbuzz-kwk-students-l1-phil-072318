085f57641821f3ec4f5da27f5be772411b7d9e47
shaileygulrajani <shaileygulrajani@gmail.com>
2018-07-24 19:06:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
-def fizzbuzz(value)
+def fizzbuzz(int)
+  if int % 3 == 0 
+    return "Fizz" 
+    
+  if int % 5 == 0 
+    return "Buzz"
+    
+  if int % 3 == 0 && int % 5 == 0
+    return "Fizzbuzz"
   
   
   
